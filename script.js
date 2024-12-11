fb16983f354330b3da02903200d552a0836bc192
mrmarioi <ivanovic.mario@gmail.com>
2024-12-11 12:45:18 +0100

    :fire: added exercice + manip DOM :fire:

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
 // ID
-
 // querySelector permet de cibler le premier élément qu'on souhaite, ici le premier élément ayant l'id "title";
 const oneId = document.querySelector('#title');
-
 // querySelectorAll permet de cibler tous les éléments ayant l'id "title";
 const allIds = document.querySelectorAll('#title');
-
 // getElementById permet de cibler l'élément ayant pour id "title".
 const titleId = document.getElementById('title');
 
 // CLASSE
-
-
 const oneClass = document.querySelector('.secondTitle');
-
 const allClasses = document.querySelectorAll('.secondTitle');
-
 const secondTitleClass = document.getElementsByClassName('secondTitle');
+const headingTitle = document.querySelector('.title_heading_article');
+const parent = document.querySelector('.parent');
 
 
+// MODIFICATIONS
+headingTitle.textContent = " CECI EST MON NOUVEAU TITRE DE TEST, TOUT LE MONDE VOIT ????????!!!!!"
+headingTitle.style.color = 'red';
+headingTitle.style.backgroundColor = 'green';
+
+// CREATION NOUVEAU ELEMENT
+const nouvelElement = document.createElement('img');
+nouvelElement.src = "https://www.economie-news.com/wp-content/uploads/2024/06/plus-belle-plage-d-europe_6679e084e11a5.jpg";
+parent.appendChild(nouvelElement);
+const paragraph = document.createElement('p');
+paragraph.textContent = 'TEXTE DANS DIV PARENT';
+parent.appendChild(paragraph);
+
+console.log(paragraph.parentElement);
+console.log(parent.children)
+
+// AJOUT DE CLASSE
+// parent.classList.add('classTest')
+// parent.classList.remove('classTest')
+// parent.classList.toggle('parent')
+// parent.classList.contains('parent')
+
+
+// SUPPRESSION D'UN ELEMENT
+parent.removeChild(nouvelElement)
 
 // #title = id="title"
 console.log("one id =>", oneId);
