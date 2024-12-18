d20e89b5485690844005380bff65fdc5a52efd16
mrmarioi <ivanovic.mario@gmail.com>
2024-12-18 09:06:29 +0100

    :fire: event, add through JS from array, fetch :fire:

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ const allClasses = document.querySelectorAll('.secondTitle');
 const secondTitleClass = document.getElementsByClassName('secondTitle');
 const headingTitle = document.querySelector('.title_heading_article');
 const parent = document.querySelector('.parent');
+const machin = document.getElementById('barfoo')
+const listeCourse = document.querySelector('#liste');
 
 
 // MODIFICATIONS
@@ -20,15 +22,12 @@ headingTitle.style.color = 'red';
 headingTitle.style.backgroundColor = 'green';
 
 // CREATION NOUVEAU ELEMENT
-const nouvelElement = document.createElement('img');
-nouvelElement.src = "https://www.economie-news.com/wp-content/uploads/2024/06/plus-belle-plage-d-europe_6679e084e11a5.jpg";
-parent.appendChild(nouvelElement);
-const paragraph = document.createElement('p');
-paragraph.textContent = 'TEXTE DANS DIV PARENT';
-parent.appendChild(paragraph);
+// const paragraph = document.createElement('p');
+// paragraph.textContent = 'TEXTE DANS DIV PARENT';
+// parent.appendChild(paragraph);
 
-console.log(paragraph.parentElement);
-console.log(parent.children)
+// console.log(paragraph.parentElement);
+// console.log(parent.children)
 
 // AJOUT DE CLASSE
 // parent.classList.add('classTest')
@@ -38,7 +37,7 @@ console.log(parent.children)
 
 
 // SUPPRESSION D'UN ELEMENT
-parent.removeChild(nouvelElement)
+// parent.removeChild(nouvelElement)
 
 // #title = id="title"
 console.log("one id =>", oneId);
@@ -54,9 +53,92 @@ const listOfNodes = Array.from(allClasses);
 const array = listOfNodes.map(l => l.textContent);
 
 console.log("nodeListe après transformation en array =>", array)
-
+console.log("MACHIN ::: ", machin);
 allClasses.forEach(c => {
     c.addEventListener('click', () => {
         c.classList.add('test');
     });
 });
+
+
+const produits = ['Pommes', 'Poires', 'Carottes', 'Peches', 'Abricots', 'Fraises'];
+
+produits.forEach((produit, index) => {
+    const li = document.createElement('li');
+    li.textContent = produit;
+    listeCourse.appendChild(li);
+})
+const liste = document.querySelector('#liste')
+console.log("ICI :", listeCourse);
+
+
+const draggable = document.querySelector('.draggable');
+const dropzone = document.querySelector('.dropzone');
+
+draggable.addEventListener('dragstart', (event) => {
+    event.target.classList.add('dragging');
+});
+
+draggable.addEventListener('dragend', (event) => {
+    event.target.classList.remove('dragging');
+});
+
+dropzone.addEventListener('dragover', (event) => {
+    event.preventDefault(); // Nécessaire pour permettre le drop
+});
+
+dropzone.addEventListener('drop', (event) => {
+    event.preventDefault();
+    const dragging = document.querySelector('.dragging');
+    event.target.appendChild(dragging);
+});
+
+
+const form = document.querySelector('#monFormulaire');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const email = form.querySelector('#email').value;
+    const password = form.querySelector('#password').value;
+
+    console.log("Données:", { email, password });
+});
+
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', () => { console.log("Button cliqué") })
+
+
+// const users = document.querySelector('#users');
+
+fetch('https://fakestoreapi.com/products?limit=30')
+    .then(response => response.json())
+    .then(products => {
+        const listOfProducts = document.querySelector('#users');
+        products.forEach(product => {
+            const displayProduct = document.createElement('div');
+            displayProduct.classList.add('test')
+            displayProduct.innerHTML = `<h3> Product : ${product.title}</h3>
+            <p> ${product.description}</p>
+            <img src=${product.image} alt=${product.title}>`;
+            listOfProducts.appendChild(displayProduct);
+        })
+    })
+    .catch(error => console.log(error))
+
+
+// produits.forEach((data, index) => {
+//     const user = document.createElement('li');
+//     user.textContent = data;
+//     users.appendChild(li);
+// })
+
+// click
+// dblclick
+// mouseover 
+// mouseout
+// keyup 
+// keydown 
+// submit 
+// change 
